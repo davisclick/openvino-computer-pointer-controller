7e76ffaecdedb4f468836490aa458e1b7e6a9883
Davis Alvarez <alvarez.davis@gmail.com>
2020-07-04 12:39:25 -0500

    Update facial_landmarks_detection.py

--- src/facial_landmarks_detection.py
+++ src/facial_landmarks_detection.py
@@ -1,10 +1,10 @@
 import os
 import cv2
-import numpy as np
+import math
 import logging
 from openvino.inference_engine import IENetwork, IECore
 
-class FacialLandmarksDetection:
+class GazeEstimation:
     '''
     Class for the Face Detection Model.
     '''
@@ -31,7 +31,7 @@ class FacialLandmarksDetection:
 
 
     def load_model(self):
-
+        
         self.plugin = IECore()
         self.network = self.plugin.read_network(model=self.model_structure, weights=self.model_weights)
         
@@ -40,40 +40,23 @@ class FacialLandmarksDetection:
         
         self.exec_network = self.plugin.load_network(network=self.model, device_name=self.device, num_requests=1)
 
-    def predict(self, image):
+    def predict(self, left_eye, right_eye, head_position):
 
-        input_img = self.preprocess_input(image)
-        input_dict={self.input_name: input_img}  
+        processed_left_eye = self.preprocess_input(left_eye)
+        processed_right_eye = self.preprocess_input(right_eye)
         
-        h, w = image.shape[:2]
+        input_dict= {'left_eye_image': processed_left_eye,
+                                              'right_eye_image': processed_right_eye,
+                                              'head_pose_angles': head_position}
+
         
-        infer_request_handle = self.exec_network.start_async(request_id=0, inputs=input_dict)
+        infer_request_handle = self.exec_network.start_async(request_id=0,inputs = input_dict)
+        
         infer_status = infer_request_handle.wait()
-
         if infer_status == 0:
-            outputs = infer_request_handle.outputs[self.output_name]
-            coords = self.preprocess_output(outputs)
-
-            coords = coords* np.array([w, h, w, h])
-            coords = coords.astype(np.int32)
+            outputs = infer_request_handle.outputs[self.output_name].tolist()[0]
             
-            (lefteye_x, lefteye_y, righteye_x, righteye_y) = coords
-
-            le_xmin = lefteye_x - 10
-            le_ymin = lefteye_y - 10
-            le_xmax = lefteye_x + 10
-            le_ymax = lefteye_y + 10
-            
-            re_xmin = righteye_x - 10
-            re_ymin = righteye_y - 10
-            re_xmax = righteye_x + 10
-            re_ymax = righteye_y + 10
-        
-            left_eye =  image[le_ymin:le_ymax, le_xmin:le_xmax]
-            right_eye = image[re_ymin:re_ymax, re_xmin:re_xmax]
-            eye_coords = [[le_xmin,le_ymin,le_xmax,le_ymax], [re_xmin,re_ymin,re_xmax,re_ymax]]
-
-            return left_eye, right_eye, eye_coords
+            return self.preprocess_output(outputs, head_position)
 
     def check_model(self):
 
@@ -99,18 +82,21 @@ class FacialLandmarksDetection:
 
     def preprocess_input(self, image):
         
-        image = cv2.resize(image, (self.input_shape[3], self.input_shape[2]))
-        image = image.transpose((2,0,1))
-        image = image.reshape(1, 3, self.input_shape[2], self.input_shape[3])
-        return image
+        net_input_shape = self.network.inputs['right_eye_image'].shape
+        p_frame = cv2.resize(image, (net_input_shape[3], net_input_shape[2]))
+        p_frame = p_frame.transpose(2, 0, 1)
+        p_frame = p_frame.reshape(1, *p_frame.shape)
+        return p_frame
 
-    def preprocess_output(self, outputs):
+    def preprocess_output(self, outputs, head_position):
 
-        #out = outputs[self.output_name][0]
-        
-        left_eye_x_coordinate  = outputs[0][0].tolist()[0][0]
-        left_eye_y_coordinate  = outputs[0][1].tolist()[0][0]
-        right_eye_x_coordinate  = outputs[0][2].tolist()[0][0]
-        right_eye_y_coordinate  = outputs[0][3].tolist()[0][0]
+        roll = head_position[2]
+        gaze_vector = outputs
 
-        return (left_eye_x_coordinate , left_eye_y_coordinate , right_eye_x_coordinate , right_eye_y_coordinate )
+        cos_theta = math.cos(roll * math.pi / 180)
+        sin_theta = math.sin(roll * math.pi / 180)
+
+        x = outputs[0] * cos_theta + outputs[1] * sin_theta
+        y = outputs[1] * cos_theta - outputs[0] * sin_theta
+
+        return (x, y), gaze_vector
